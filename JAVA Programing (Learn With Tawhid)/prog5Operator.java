1c1257bb8f042665c353e4fd78da2f847b0f0199
Delowar-Designer <delowar.designer@gmail.com>
2024-06-02 17:46:37 +0600

    Aritmetic Operators in Java

---
+++ JAVA Programing (Learn With Tawhid)/prog5Operator.java
@@ -0,0 +1,18 @@
+public class prog5Operator
+{
+  public static void main(String [] args)
+  {
+    System.out.println(10 + 13);
+    System.out.println(10.0 + 13);
+    System.out.println(50 - 13);
+    System.out.println(50 - 13.0);
+    System.out.println(5 * 3);
+    System.out.println(9 / 3);
+    System.out.println(9.0 / 2);
+    System.out.println(9 / 2);
+    System.out.println(9 / 2.0);
+    System.out.println(19 % 6);
+    System.out.println(9 % 2);
+    System.out.println(2 % 7);
+  }
+}
