f788c7173002ca75657eba3a295b81ce9c0b699a
Delowar-Designer <delowar.designer@gmail.com>
2024-07-04 17:02:51 +0600

    For Loop

---
+++ JAVA Programing (Learn With Tawhid)/Java13_For_Loop.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Java13_For_Loop
+{
+  public static void main (String [] args)
+//  {
+//    for(int i=1; i<=50; i++){
+//      System.out.println(i);
+//    }
+//   {
+//    for(int i=12; i>=5; i--){
+//      System.out.println(i);
+//    }
+//    {
+//      for(int i=1; i<=50; i++){
+//        if(i%2==0){
+//          System.out.println(i + "Even");
+//        }
+//      else{
+//        System.out.println(i + "Odd");
+//      }
+       {
+      for(int i=81; i>40; i--){
+        if(i%6==0){
+          System.out.println(i + " is divisible by 6");
+        }
+     }
+  }
+}
---
+++ JAVA Programing (Learn With Tawhid)/Java13_For_Loop.java~
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Java12_While_Loop
+{
+  public static void main (String [] args)
+  {
+    int i = 0;                    //Initialization
+    while (i<=50){                //condition
+      System.out.println(i);      //Body
+      i += 1;                     //steps
+   } 
+  }
+}
