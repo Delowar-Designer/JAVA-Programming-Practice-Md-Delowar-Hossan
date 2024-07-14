2d828dcaa538c8ac54b0dc409d975be67a86d312
Delowar-Designer <delowar.designer@gmail.com>
2024-07-15 04:05:03 +0600

    Memory Locations

--- JAVA Programing (Learn With Tawhid)/Java OOP/Student.java
+++ JAVA Programing (Learn With Tawhid)/Java OOP/Student.java
@@ -2,4 +2,15 @@ public class Student
 {
   public String name = "Delowa";
   public String id;
+  
+   public void showDetail(){
+    System.out.println(name+" "+id);
+  }
+    public void standUp(){
+    System.out.println(name+" is now Standing");
+  }
+//   
+//  public void m1(int n){
+//    System.out.println(n +" Programer Delowar.Designer");
+//  }
 }
