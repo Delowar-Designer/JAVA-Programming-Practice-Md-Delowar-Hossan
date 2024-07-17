bfa034f98bd7b712a32a44f300c2f07f8e14ab13
Delowar-Designer <delowar.designer@gmail.com>
2024-07-17 16:30:08 +0600

    JAVA Programing Anisul isalm

---
+++ JAVA Programing(Anisul Islam)/BasicJava/src/main/java/basicjava/BioData.java
@@ -0,0 +1,12 @@
+
+package basicjava;
+
+public class BioData {
+    
+    public static void main(String[] args){
+        
+        System.out.println("Delowar Hossan");
+        System.out.print("01773873289");
+    }
+    
+}
