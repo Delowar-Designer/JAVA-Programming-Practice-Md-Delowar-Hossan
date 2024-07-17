e7a84408dfaa8576a685d16c8724eeab9a59ae63
Delowar-Designer <delowar.designer@gmail.com>
2024-07-17 17:48:21 +0600

    JAVA 49 Assignment1 Create Class and print data

---
+++ JAVA Programing(Anisul Islam)/BasicJava/src/main/java/basicjava/Java5_Assignment_1_Create_Class_and_print_data.java
@@ -0,0 +1,16 @@
+
+package basicjava;
+
+//Java5_Assignment_1_Create_Class_and_print_data
+public class Java5_Assignment_1_Create_Class_and_print_data {
+    
+    public static void main(String[] args){
+        System.out.println("id : 101");
+        System.out.println("title: iphone15");
+        System.out.println("priceL 1895 euros");
+        System.out.println("description: perfect product with best image quality");
+        System.out.println("categry: phone");
+        System.out.println("Programer Delowar.Designer");
+    } 
+    
+}
