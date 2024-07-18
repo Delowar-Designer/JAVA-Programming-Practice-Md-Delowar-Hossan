23e6da85e8d075eea691740d688d1e50313a6f4e
Delowar-Designer <delowar.designer@gmail.com>
2024-07-18 15:55:54 +0600

    Escape_Sequence

--- JAVA Programing(Anisul Islam)/BasicJava/src/main/java/basicjava/Java6_Escape_Sequence.java
+++ JAVA Programing(Anisul Islam)/BasicJava/src/main/java/basicjava/Java6_Escape_Sequence.java
@@ -7,8 +7,17 @@ public class Java6_Escape_Sequence {
      public static void main(String[] args){
         
         System.out.println("Delowar Hossan");
-        System.out.print("01773873289");
-        System.out.print("BSC in CSE");
+        System.out.println("01773873289");
+        System.out.println("BSC in CSE");
+        System.out.println("========================");
+        System.out.println("MD Delowar Hossan \nRoll: 602826 \nCSE");
+        System.out.println("1 \t 2 \t 3 \t 4 \n5 \t 6 \t 7 \t 8");
+        System.out.println(1);
+        System.out.println(" \"Java Programming\" ");
+        System.out.println(" \'Java Programming\' ");
+        System.out.println(" \\Java Programming\\ ");
+        System.out.println("\rJava\rProgramming");
+        System.out.println("\b Java \b Programming");
     }
     
 }
