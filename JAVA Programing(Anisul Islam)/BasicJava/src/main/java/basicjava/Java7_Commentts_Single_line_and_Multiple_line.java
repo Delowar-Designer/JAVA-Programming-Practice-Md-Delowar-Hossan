2135ffbf5785a1d29a0e8fca93a824310a873058
Delowar-Designer <delowar.designer@gmail.com>
2024-07-18 16:15:17 +0600

    Comments Single line and Multiplee line

---
+++ JAVA Programing(Anisul Islam)/BasicJava/src/main/java/basicjava/Java7_Commentts_Single_line_and_Multiple_line.java
@@ -0,0 +1,26 @@
+/*
+    Md Delowar Hossan
+    BioData Programer
+    My Skile:
+    *Pysthon programing
+    *Java Programing
+*/
+package basicjava;
+
+/**
+ *
+ * @author Programmer Delowar
+ * Java7_Commentts_Single_line_and_Multiple_line
+ */
+public class Java7_Commentts_Single_line_and_Multiple_line {
+    
+    public static void main(String [] args){
+        
+        //displaying my compane name
+        System.out.println("Programer Delowar.Designer");
+        //Documentational comment
+        //Displaying my age
+        System.out.println("19 years old");
+    }
+    
+}
