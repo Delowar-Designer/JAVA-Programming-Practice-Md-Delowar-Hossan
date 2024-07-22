7bed8cb1190e8da6eb75c698360bc0eeac86d4e1
Delowar-Designer <delowar.designer@gmail.com>
2024-07-22 16:27:14 +0600

    Assignment2 Variables and data types

---
+++ JAVA Programing(Anisul Islam)/BasicJava/src/main/java/basicjava/Java11_Assignment_2_Variables_and_data_types.java
@@ -0,0 +1,23 @@
+
+package basicjava;
+
+public class Java11_Assignment_2_Variables_and_data_types {
+    public static void main(String[] args){
+        
+        int id = 602826;
+        String title = "iphone15";
+        String price = "1895 euros";
+        String description = "perfect product with best image quality";
+        String category = "phone";
+        
+        System.out.println("id is: "+id);
+        System.out.println("Title is: "+title);
+        System.out.println("Price is: "+price);
+        System.out.println("Description is: "+description);
+        System.out.println("Category is: "+category);
+        
+        
+        
+    }
+    
+}
