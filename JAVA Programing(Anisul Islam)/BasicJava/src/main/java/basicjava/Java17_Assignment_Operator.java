acbf4d1dc21f1c45fcf146ee3da556c32cc9515d
Md Delowar Hossan <144885151+Delowar-Designer@users.noreply.github.com>
2024-08-07 12:52:13 +0300

    Assignment operator

--- JAVA Programing(Anisul Islam)/BasicJava/src/main/java/basicjava/Java17_Assignment_Operator.java
+++ JAVA Programing(Anisul Islam)/BasicJava/src/main/java/basicjava/Java17_Assignment_Operator.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package basicjava;
 
-/**
- *
- * @author Programmer Delowar
- */
+
 public class Java17_Assignment_Operator {
+    public static void main(String[] args){
+        
+        String category = "phone";
+        
+        System.out.println("id is: "+category);
+    }
     
 }
